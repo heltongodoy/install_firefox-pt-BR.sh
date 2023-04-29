d0134eefd34b47ce88e341a30c3785f2b7d0317f
helton.godoy <36382889+heltongodoy@users.noreply.github.com>
2023-04-29 17:06:04 -0400

    Update firefox_install.sh
    Melhoria da apresentação das fases de instalação.

--- firefox_install.sh
+++ firefox_install.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
+echo " "
+echo "#############################################################################################"
+echo "#                  Removendo Firefox SNAP, caso exista.                                     #"
+echo "#############################################################################################"
 sudo snap remove --purge firefox
+echo " "
+echo " "
+echo "#############################################################################################"
+echo "#                             Iniciando download.                                           #"
+echo "#############################################################################################"
 wget -O ~/FirefoxSetup.tar.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=pt-BR"
+echo " "
+echo " "
+echo "#############################################################################################"
+echo "#                            Iniciando instalação.                                          #"
+echo "#############################################################################################"
 sudo tar xjf ~/FirefoxSetup.tar.bz2 -C /opt/
 sudo rm -rf /usr/bin/firefox
 sudo ln -s /opt/firefox/firefox /usr/bin/firefox
+rm ~/FirefoxSetup.tar.bz2
+if [ -e "/usr/local/share/applications/firefox.desktop" ] ; then
+echo "O arquivo firefox.desktop existe. Finalizar"
+else
+echo "O arquivo firefox.desktop não existe. Gerar arquivo."
 sudo wget https://raw.githubusercontent.com/mozilla/sumo-kb/main/install-firefox-linux/firefox.desktop -P /usr/local/share/applications
+fi
+echo " "
+echo " "
+echo "#############################################################################################"
+echo "#                              INSTALAÇÃO FINALIZADA!                                       #"
+echo "#############################################################################################"
